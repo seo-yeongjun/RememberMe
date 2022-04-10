9a77db20f739b23f00c54a30514dfcbd163ab3f0
seo-yeongjun <zanygeek8371@gmail.com>
2022-04-10 23:11:38 +0900

    html 수정

--- src/main/java/com/zanygeek/rememberme/controller/MemorialController.java
+++ src/main/java/com/zanygeek/rememberme/controller/MemorialController.java
@@ -2,15 +2,21 @@ package com.zanygeek.rememberme.controller;
 
 import com.zanygeek.rememberme.entity.Member;
 import com.zanygeek.rememberme.entity.Memorial;
+import com.zanygeek.rememberme.entity.Wreath;
 import com.zanygeek.rememberme.repository.MemorialRepository;
+import com.zanygeek.rememberme.repository.WreathRepository;
 import com.zanygeek.rememberme.service.MemorialService;
 import com.zanygeek.rememberme.service.PhotoService;
+import com.zanygeek.rememberme.service.WreathService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("memorial")
@@ -22,6 +28,10 @@ public class MemorialController {
     PhotoService photoService;
     @Autowired
     MemorialRepository memorialRepository;
+    @Autowired
+    WreathService wreathService;
+    @Autowired
+    WreathRepository wreathRepository;
 
     //추모관 생성 get
     @GetMapping("new")
@@ -43,14 +53,29 @@ public class MemorialController {
         }
         return "redirect:/";
     }
-
+   //추모공간 get
     @GetMapping("{memorialId}")
-    public String momirlal(Model model, @PathVariable int memorialId, @SessionAttribute(name = "member", required = false) Member member) {
+    public String momirlal(Model model, @PathVariable int memorialId, @SessionAttribute(name = "member", required = false) Member member, @ModelAttribute Wreath wreath) {
         Memorial memorial = memorialRepository.findById(memorialId).orElse(null);
         if (memorial == null)
             return "error";
         model.addAttribute("memorial", memorial);
         model.addAttribute("mainImg",photoService.getMainPhoto(memorial));
         return "memorial/memorial";
+    }
+
+    //헌화하기 get
+    @GetMapping("{memorialId}/wreath")
+    public String wreath(Model model,@PathVariable int memorialId){
+        List<Wreath> wreaths = wreathRepository.findAllByMemorialIdOrderByDateDesc(memorialId);
+        model.addAttribute("wreaths", wreaths);
+        return "memorial/flowers";
+    }
+
+    //헌화하기 post
+    @PostMapping("{memorialId}/wreath")
+    public String wreath(Model model, Wreath wreath, @PathVariable int memorialId){
+        wreathService.saveWreath(wreath, memorialId);
+        return "redirect:/memorial/"+memorialId+"#wreath";
     }
 }
